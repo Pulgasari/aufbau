caf95f9f6f6f7abe47d2f5753959cc5226750d31
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 07:22:07 +0200

    Update worker.js

--- packages/stylesheet/plugins/worker.js
+++ packages/stylesheet/plugins/worker.js
@@ -6,11 +6,11 @@ const CACHE_NAME        = 'aufbau-stylesheet-cache-v1';
 const TARGET_EXTENSIONS = ['.aufbau.css', '.ass'];
 const REGEX_TARGET_EXT  = new RegExp(`(${TARGET_EXTENSIONS.map(ext => ext.replace('.', '\\.')).join('|')})$`, 'i');
 
-function shouldIntercept (url) {
+function shouldIntercept(url) {
   return REGEX_TARGET_EXT.test(new URL(url).pathname);
 }
 
-export function setupServiceWorker () {
+export function setupServiceWorker() {
   self.addEventListener('fetch', (event) => {
     const request = event.request;
 
@@ -28,9 +28,8 @@ export function setupServiceWorker () {
 
             const rawCode        = await networkResponse.text();
             const transformedCss = transform(rawCode);
-            const cssResponse    = new Response(transformedCss, { headers: { 'Content-Type': 'text/css; charset=utf-8' }});   
+            const cssResponse    = new Response(transformedCss, { headers: { 'Content-Type': 'text/css; charset=utf-8' } });
 
-            // Put in Cache Storage
             cache.put(request, cssResponse.clone());
             return cssResponse;
           })
@@ -39,7 +38,6 @@ export function setupServiceWorker () {
             return cachedResponse || new Response('/* Aufbau SW Fetch Error */', { status: 500 });
           });
 
-        // Stale-While-Revalidate: Return cached immediately if available, else wait for fetch
         return cachedResponse || fetchAndTransform;
       })()
     );
