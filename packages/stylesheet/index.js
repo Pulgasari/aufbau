65e77f5333c70f90c14beb3d68b4216c8632cf23
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 07:08:18 +0200

    Update index.js

--- packages/stylesheet/index.js
+++ packages/stylesheet/index.js
@@ -1,13 +1,13 @@
 // @aufbau/stylesheet
 
-import { extractTokens, transformTokenProperties, transformAufbauColors } from './tokens.js';
-import { observeDom }    from './client.js';
-import transformCenter   from './center.js';
-import transformIcons    from './icon.js';
-import transformLayouts  from './layout.js';
-import transformMedia    from './media.js';
-import transformWebfonts from './webfont.js';
-import { transformFlex, transformGrid } from './layout.js';
+import { extractTokens, transformTokenProperties, transformAufbauColors } from './skills/tokens.js';
+import { observeDom }    from './plugins/client.js';
+import transformCenter   from './skills/center.js';
+import transformIcons    from './skills/icon.js';
+import transformLayouts  from './skills/layout.js';
+import transformMedia    from './skills/media.js';
+import transformWebfonts from './skills/webfont.js';
+import { transformFlex, transformGrid } from './skills/layout.js';
 
 // :::::: pre-compiled RegExp rules
 
