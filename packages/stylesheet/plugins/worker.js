f0f56ec25e846114ea9b9b2efbc75fbe1591ae19
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 03:31:15 +0200

    Update and rename packages/stylesheet/worker.js to packages/stylesheet/plugins/worker.js

--- packages/stylesheet/plugins/worker.js
+++ packages/stylesheet/plugins/worker.js
@@ -1,25 +1,18 @@
-// @aufbau/packages/stylesheet/src/worker.js
+// @aufbau/stylesheet/plugins/worker.js
 
 import transform from './index.js';
 
 // Dateiendungen, die abgefangen werden
-const TARGET_EXTENSIONS = [
-  '.aufbau.css',
-  '.ass'
-];
+const TARGET_EXTENSIONS = ['.aufbau.css', '.ass'];
 
-/**
- * Prüft, ob ein Request abgefangen werden soll
- */
-function shouldIntercept(url) {
+// Prüft, ob ein Request abgefangen werden soll
+function shouldIntercept (url) {
   const pathname = new URL(url).pathname;
   return TARGET_EXTENSIONS.some(ext => pathname.endsWith(ext));
 }
 
-/**
- * Service Worker Event Listener Registrierung
- */
-export function setupServiceWorker() {
+// Service Worker Event Listener Registrierung
+export function setupServiceWorker () {
   self.addEventListener('fetch', (event) => {
     const request = event.request;
 
@@ -29,10 +22,10 @@ export function setupServiceWorker() {
           .then(response => response.text())
           .then(rawCode => {
             // Transformiert Aufbau CSS -> Standard CSS
-            const transformedCss = transform(rawCode);
+            const transformedCSS = transform(rawCode);
 
             // Antworte dem Browser direkt mit Content-Type text/css
-            return new Response(transformedCss, {
+            return new Response(transformedCSS, {
               headers : {
                 'Content-Type' : 'text/css; charset=utf-8'
               }
