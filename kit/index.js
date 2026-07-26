52965fdcb07e4b3f549a05845c69cabbd0fbe0c6
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 11:40:44 +0200

    Update index.js

--- kit/index.js
+++ kit/index.js
@@ -4,7 +4,7 @@
 import { render, h, Component, createContext, cloneElement } from 'preact';
 import { useState, useEffect, useMemo, useCallback, useRef, useContext } from 'preact/hooks';
 import { signal, computed, effect, batch, useSignal, useComputed, useSignalEffect } from '@preact/signals';   
-import { h } from 'preact'; import htm from 'htm'; export const html = htm.bind(h);
+import htm from 'htm'; export const html = htm.bind(h);
 export {
   render, h, Component, createContext, cloneElement,
   useState, useEffect, useMemo, useCallback, useRef, useContext,
