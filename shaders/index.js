7d647c7cc3b328ff7607fd93726e4f801e307560
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-26 07:51:23 +0200

    Create index.js

---
+++ shaders/index.js
@@ -0,0 +1,24 @@
+// @aufbau/shaders
+
+import waveShader       from './presets/wave.js';
+import heavyCyberGlitch from './presets/heavy_cyber_glitch.js';
+import { glitchRgb, liveGlitch } from './presets/glitch.js';
+
+/**
+ * Registry mapping shader names to their respective SVG generator functions.
+ */
+export const presets = {
+  'wave'               : waveShader,
+  'wave-shader'        : waveShader,
+  'glitch-rgb'         : glitchRgb,
+  'live-glitch'        : liveGlitch,
+  'heavy-cyber-glitch' : heavyCyberGlitch,
+  'cyber-glitch'       : heavyCyberGlitch,
+};
+
+export {6
+  waveShader,
+  glitchRgb,
+  liveGlitch,
+  heavyCyberGlitch
+};
