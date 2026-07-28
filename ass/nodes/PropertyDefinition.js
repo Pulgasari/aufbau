3a5610cd20f635f58c9e90bc7fc8d9f9689eca3b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 10:20:37 +0200

    Update PropertyDefinition.js

--- ass/nodes/PropertyDefinition.js
+++ ass/nodes/PropertyDefinition.js
@@ -3,9 +3,19 @@ export default class PropertyDefinition {
     this.ass = ass;
     this.name = name;
     this.config = config;
+    this.registered = false;
   }
   get cssName () {
     return this.config.css ?? this.config.name ?? this.name;
+  }
+  get syntax () {
+    return this.config.syntax;
+  }
+  get initial () {
+    return this.config.initial;
+  }
+  get inherits () {
+    return this.config.inherits ?? true;
   }
   format (value) {
     if (typeof value !== "number") {
@@ -16,4 +26,30 @@ export default class PropertyDefinition {
     }
     return `${value}${this.config.unit}`;
   }
-}
+  canRegister () {
+    return Boolean(
+      this.cssName.startsWith("--") &&
+      this.syntax &&
+      this.initial !== undefined
+    );
+  }
+  register () {
+    if (this.registered) {
+      return this;
+    }
+    if (!this.canRegister()) {
+      return this;
+    }
+    if (!globalThis.CSS?.registerProperty) {
+      return this;
+    }
+    CSS.registerProperty({
+      name: this.cssName,
+      syntax: this.syntax,
+      inherits: this.inherits,
+      initialValue: this.format(this.initial)
+    });
+    this.registered = true;
+    return this;
+  }
+},
