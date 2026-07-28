8a38c0c7ddb61de4351a2b794bcebba8a17678f1
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 11:15:00 +0200

    Update ast.js

--- ass/parser/ast.js
+++ ass/parser/ast.js
@@ -14,3 +14,16 @@ export default function createAtRuleAST (
     }
   );
 }
+
+export default function createDeclarationAST (
+  property,
+  value
+) {
+  return createNodeAST(
+    "declaration",
+    {
+      property,
+      value
+    }
+  );
+}
