5016070494000947fa1c641c338a157ff5501ea1
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 11:28:05 +0200

    Update RuleNode.js

--- ass/nodes/RuleNode.js
+++ ass/nodes/RuleNode.js
@@ -3,10 +3,14 @@ import ASSNode from "../core/ASSNode.js";
 export default class RuleNode extends ASSNode {
 
   constructor (ass, selector) {
-    super(ass, "rule", selector);
+    super(
+      ass,
+      "rule",
+      selector
+    );
     this.data.selector = selector;
   }
-
+  
   get selector () {
     return this.data.selector;
   }
