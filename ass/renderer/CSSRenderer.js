716e875ad439887040931e6e1308d059651c0c1a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 11:21:38 +0200

    Update CSSRenderer.js

--- ass/renderer/CSSRenderer.js
+++ ass/renderer/CSSRenderer.js
@@ -1,9 +1,20 @@
 import Renderer from "./Renderer.js";
 
 export default class CSSRenderer extends Renderer {
+  renderAtRule (node) {
+    const body = node.children
+      .map(child => this.visit(child))
+      .join("\n");
+    if (!body) {
+      return `@${node.name} ${node.prelude};`;
+    }
+    return `@${node.name} ${node.prelude} {\n${body}\n}`;
+  }
+  
   renderSheet (node) {
     return node.children.map(child => this.visit(child)).join("\n");
   }
+  
   renderRule (node) {
     const body = node.children.map(child => this.visit(child)).join("\n");
     return `${node.selector} {\n${body}\n}`;
