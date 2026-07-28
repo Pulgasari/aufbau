26177aa8a5380366b857de0e1f7f66801eec3a4b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 11:04:12 +0200

    Update grammar.js

--- ass/parser/grammar.js
+++ ass/parser/grammar.js
@@ -1,6 +1,26 @@
-import { many, map } from "@cosmonaut/blocks";
+import { many, map, seq, token } from "@cosmonaut/blocks";
 
 import rule from "./rule.js";
+import value from "./value.js";
+
+import createDeclarationAST from "../ast/DeclarationAST.js";
+
+export default function declaration () {
+  return map(
+    seq(
+      token("IDENTIFIER"),
+      token("COLON"),
+      value(),
+      token("SEMICOLON")
+    ),
+    ([property, , value]) => {
+      return createDeclarationAST(
+        property.value,
+        value
+      );
+    }
+  );
+}
 
 export default function stylesheet () {
   return map(
