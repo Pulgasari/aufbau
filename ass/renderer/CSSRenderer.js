ce8227ca420dc0bdbeab68d90bbbef35292ce1e3
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 10:12:04 +0200

    Create CSSRenderer.js

---
+++ ass/renderer/CSSRenderer.js
@@ -0,0 +1,25 @@
+import Renderer from "./Renderer.js";
+
+export default class CSSRenderer extends Renderer {
+  renderSheet (node) {
+    return node.children.map(child => this.visit(child)).join("\n");
+  }
+  renderRule (node) {
+    const body = node.children.map(child => this.visit(child)).join("\n");
+    return `${node.selector} {\n${body}\n}`;
+  }
+  renderDeclaration (node) {
+    return `  ${node.property}: ${node.value};`;
+  }
+  renderProperty (node) {
+    return this.renderDeclaration(node);
+  }
+  renderMedia (node) {
+    const body = node.children.map(child => this.visit(child)).join("\n");
+    return `@media ${node.query} {\n${body}\n}`;
+  }
+  renderLayer (node) {
+    const body = node.children.map(child => this.visit(child)).join("\n");
+    return `@layer ${node.name} {\n${body}\n}`;
+  }
+                                                                    }
