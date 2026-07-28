9547d5326f87668efd1194a49900df8aeb29c2f3
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 10:18:11 +0200

    Update ASS.js

--- ass/ASS.js
+++ ass/ASS.js
@@ -2,16 +2,21 @@ import CSSRenderer from "./renderer/CSSRenderer.js";
 import EventEmitter from "./core/EventEmitter.js";
 import Scheduler    from "./core/Scheduler.js";
 import SheetNode    from "./nodes/SheetNode.js";
+import PropertyRegistry from "./registries/PropertyRegistry.js";
 
 export default class ASS {
   #events;
+  #properties;
+  #renderer;
   #scheduler;
   #sheet;
+  
   constructor () {
-    this.#events    = new EventEmitter();
-    this.#scheduler = new Scheduler(() => { this.render(); });
-    this.#renderer  = new CSSRenderer();
-    this.#sheet     = new SheetNode(this);
+    this.#events     = new EventEmitter();
+    this.#scheduler  = new Scheduler(() => { this.render(); });
+    this.#properties = new PropertyRegistry(this);
+    this.#renderer   = new CSSRenderer();
+    this.#sheet      = new SheetNode(this);
   }
   get sheet () {
     return this.#sheet;
@@ -30,6 +35,15 @@ export default class ASS {
   }
   prop (name, value) {
     return this.#sheet.prop(name, value);
+  }
+  props (definitions) {
+    for (const [name, config] of Object.entries(definitions)) {
+      this.#properties.register(name, config);
+    }
+    return this;
+  }
+  property (name) {
+    return this.#properties.resolve(name);
   }
   dirty () {
     this.#scheduler.queue();
