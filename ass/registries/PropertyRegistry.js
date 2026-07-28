138588804d7a6734e1a70d7c77123facd951b5e6
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 10:16:39 +0200

    Update PropertyRegistry.js

--- ass/registries/PropertyRegistry.js
+++ ass/registries/PropertyRegistry.js
@@ -1,18 +1,29 @@
 import Registry from "../core/Registry.js";
 import Property from "../objects/Property.js";
+import { normalizeName } from "../core/normalize.js";
+import PropertyDefinition from "../nodes/PropertyDefinition.js";
 
-export default class PropertyRegistry extends Registry {
 
-    register(name, config = {}) {
-
-        config.name ??= name;
-
-        const prop = new Property(this.ass, config);
-
-        this.add(name, prop);
-
-        return prop;
-
+export default class PropertyRegistry {
+  #ass;
+  #properties = new Map();
+  constructor (ass) {
+    this.#ass = ass;
+  }
+  register (name, config = {}) {
+    const property = new PropertyDefinition(this.#ass, name, config);
+    this.#properties.set(normalizeName(name), property);
+    if (config.aliases) {
+      for (const alias of config.aliases) {
+        this.#properties.set(normalizeName(alias), property);
+      }
     }
-
+    return property;
+  }
+  resolve (name) {
+    return this.#properties.get(normalizeName(name));
+  }
+  has (name) {
+    return this.#properties.has(normalizeName(name));
+  }
 }
