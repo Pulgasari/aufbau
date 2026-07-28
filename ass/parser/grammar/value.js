5b2ec167cb98f62819664c581e4b071c6436f90b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 10:59:41 +0200

    Update value.js

--- ass/parser/grammar/value.js
+++ ass/parser/grammar/value.js
@@ -1,16 +1,18 @@
-import { any, many1, map } from "@cosmonaut/blocks";
+import { any, choice, many1, map, token } from "@cosmonaut/blocks";
 
 export default function value () {
-
   return map(
-    many1(any()),
+    many1(
+      choice(
+        token("VALUE"),
+        token("IDENTIFIER"),
+        token("CHAR")
+      )
+    ),
     tokens => {
-
       return tokens
         .map(token => token.value)
         .join("");
-
     }
   );
-
 }
