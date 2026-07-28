6a26af1d60874da7315d3a7e03d21c6344477c05
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 15:18:11 +0200

    Update cascade.js

--- ass/cascade.js
+++ ass/cascade.js
@@ -74,13 +74,3 @@ class StyleResolver {
 
 
 
-export default class ComputedStyle {
-  custom     = {};
-  inherited  = {};
-  properties = {};
-  
-  get (name)       { return this.properties[name];  }
-  set (name,value) { this.properties[name] = value; }
-}
-
-
