1993d2a4eb472950eab12ac134abd5b24388f724
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-28 11:13:09 +0200

    Update NodeBuilder.js

--- ass/core/NodeBuilder.js
+++ ass/core/NodeBuilder.js
@@ -1,6 +1,34 @@
 import DeclarationNode from "../nodes/DeclarationNode.js";
+import     RuleNode from "../nodes/RuleNode.js";
+import    MediaNode from "../nodes/MediaNode.js";
+import    LayerNode from "../nodes/LayerNode.js";
+import PropertyNode from "../nodes/PropertyNode.js";
 
 export default function NodeBuilder (node) {
+  node.atRule = function (name, prelude = "") {
+    const rule = new AtRuleNode(
+      this.ass,
+      name,
+      prelude
+    );
+    this.append(rule);
+    return rule;
+  };
+  node.rule = function (selector) {
+    const rule = new RuleNode(this.ass, selector);
+    this.append(rule);
+    return rule;
+  };
+  node.layer = function (name) {
+    const layer = new LayerNode(this.ass, name);
+    this.append(layer);
+    return layer;
+  };
+  node.media = function (query) {
+    const media = new MediaNode(this.ass, query);
+    this.append(media);
+    return media;
+  };
   node.prop = function (name, value) {
     const definition = this.ass.property(name);
     const property = definition ?? {
@@ -15,35 +43,5 @@ export default function NodeBuilder (node) {
     );
     this.append(declaration);
     return declaration;
-  };
-}
-
-import     RuleNode from "../nodes/RuleNode.js";
-import    MediaNode from "../nodes/MediaNode.js";
-import    LayerNode from "../nodes/LayerNode.js";
-import PropertyNode from "../nodes/PropertyNode.js";
-
-export default function NodeBuilder (node) {
-  node.rule = function (selector) {
-    const rule = new RuleNode(this.ass, selector);
-    this.append(rule);
-    return rule;
-  };
-  node.media = function (query) {
-    const media = new MediaNode(this.ass, query);
-    this.append(media);
-    return media;
-  };
-  node.layer = function (name) {
-    const layer = new LayerNode(this.ass, name);
-    this.append(layer);
-    return layer;
-  };
-  node.prop = function (name, value) {
-    const prop = new PropertyNode(this.ass, name, {
-      initial: value
-    });
-    this.append(prop);
-    return prop;
   };
 }
