e54a04948bae89c8d533025f5dcb8fbd708a5a3f
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-29 18:54:53 +0200

    Update registry.js

--- classcade/registry.js
+++ classcade/registry.js
@@ -8,8 +8,8 @@ class Registry {
   variants : new Map;
 
   // to register
-  rule    (obj) { this.rules   .set(obj.id, obj); }
   method  (obj) { this.methods .set(obj.id, obj); }
+  rule    (obj) { this.rules   .set(obj.id, obj); }
   variant (obj) { this.variants.set(obj.id, obj); }
 };
 
