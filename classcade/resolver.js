95802e65d5c7d13bbad77cc35b3c8343a0c8e044
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-29 21:20:14 +0200

    Create resolver.js

---
+++ classcade/resolver.js
@@ -0,0 +1,46 @@
+// resolver.js
+
+class Resolver {
+  
+  resolve (ast) {
+    if (!Array.isArray(ast)) ast = [ast];
+    return ast.map(node => this.resolveNode(node));
+  }
+
+  resolveNode () {
+    switch (node.type) {
+      case 'method'  : return this.resolveMethod  (node);
+      case 'rule'    : return this.resolveRule    (node);
+      case 'variant' : return this.resolveVariant (node);
+    }
+  }
+
+  resolveMethod (node) {
+    const method = this.registry.getMethod(node.id);
+    const args   = node.args.map (arg => {
+      if (typeof arg === 'object') return this.resolveNode(arg);
+      return arg;
+    });
+    return method.run(...args);
+  }
+
+  resolveRule (node) {
+    const rule = this.registry.getRule(node.id);
+    const args = node.args.map (
+      arg => (typeof arg === 'object') ? this.resolveNode(arg) : arg;
+    );
+    const declarations = rule.css(...args);
+
+    return {
+      id       : node.raw,
+      selector : node.selector,
+      declarations,
+      media    : null,
+      supports : null,
+      layer    : null,
+    };
+  }
+  
+}
+
+export default Resolver;
