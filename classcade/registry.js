fbd0a82dda18b6dfc24c43e2f7a3fe41f92f83ee
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-29 18:33:37 +0200

    Create registry.js

---
+++ classcade/registry.js
@@ -0,0 +1,11 @@
+// registry.js
+
+const registry = {
+  rules     : new Map,
+  functions : new Map,
+  variants  : new Map,
+  macros    : new Map,
+  themes    : new Map,
+};
+
+export default registry;
