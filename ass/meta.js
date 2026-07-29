b3fb17e95c5a4290a292769d15f9a2acb54ccedd
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-29 14:08:34 +0200

    Create meta.js

---
+++ ass/meta.js
@@ -0,0 +1,67 @@
+// meta.js
+
+const subPropsmap = {
+  margin  : ['bottom', 'left', 'right', 'top'],
+  padding : ['bottom', 'left', 'right', 'top'],
+
+  height : ['max', 'min'],
+  width  : ['max', 'min'],
+
+  gap : ['column', 'row'],
+};
+
+const propsMap = {
+  // ASS
+  colors  : null,
+  pattern : null,
+  shader  : null,
+  webfont : null,
+  // CSS
+  border   : null,
+  bottom   : { unit: 'px' },
+  flex     : null,
+  fontSize : { unit: 'px' },
+  gap      : { unit: 'px' },
+  height   : { unit: 'px' },
+  left     : { unit: 'px' },
+  margin   : { unit: 'px' },
+  opacity  : { unit: null },
+  outline  : null,
+  padding  : { unit: 'px' },
+  right    : { unit: 'px' },
+  top      : { unit: 'px' },
+  width    : { unit: 'px' },
+  zIndex   : { unit: false },
+};
+
+export const defaultUnits = {
+  fontSize     : 'px',
+  width        : 'px',
+  height       : 'px',
+  minWidth     : 'px',
+  maxWidth     : 'px',
+  minHeight    : 'px',
+  maxHeight    : 'px',
+  margin       : 'px',
+  marginTop    : 'px',
+  marginRight  : 'px',
+  marginBottom : 'px',
+  marginLeft   : 'px',
+  padding      : 'px',
+  paddingTop   : 'px',
+  paddingRight : 'px',
+  paddingBottom: 'px',
+  paddingLeft  : 'px',
+  gap          : 'px',
+  rowGap       : 'px',
+  columnGap    : 'px',
+  borderRadius : 'px',
+  top          : 'px',
+  right        : 'px',
+  bottom       : 'px',
+  left         : 'px',
+  borderWidth  : 'px',
+  opacity      : '',
+  zIndex       : '',
+  flex         : ''
+};
