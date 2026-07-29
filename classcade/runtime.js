fb18a17ffb73d975b79a0e5c28bffcdfc037507a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-29 21:54:05 +0200

    Update runtime.js

--- classcade/runtime.js
+++ classcade/runtime.js
@@ -1,6 +1,32 @@
 // runtime.js
 
-class Runtime {
+export class Scanner {
+
+  constructor (options = {}) {
+    this.attributes = options.attributes ?? ['cc', 'class', 'classcade', 'className', 'data-classcade'];    
+  }
+
+  scan (root = document) {
+    const entries = [];
+
+    // check root + children
+    if (root.nodeType === 1) this.scanNode(root, entries);
+    const selector = this.attributes.map(attr => `[${attr}]`).join(', ');
+    root.querySelectorAll(selector).forEach(node => this.scanNode(node, entries));
+
+    return entries;
+  }
+
+  scanNode (node, entries) {
+    for (const attribute of this.attributes) {
+      if (!node.hasAttribute(attribute)) continue;
+      entries.push({ node, attribute, value: node.getAttribute(attribute) });
+    }
+  }
+
+}
+
+export class Runtime {
 
   constructor (classcade) {
     this.classcade = classcade;
