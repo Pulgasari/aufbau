995cb272d7a7b2e892e2c08b6c8a9edb493010bb
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-29 18:54:32 +0200

    Update registry.js

--- classcade/registry.js
+++ classcade/registry.js
@@ -1,11 +1,16 @@
 // registry.js
 
-const registry = {
-  rules     : new Map,
-  functions : new Map,
-  variants  : new Map,
-  macros    : new Map,
-  themes    : new Map,
+class Registry {
+  macros   : new Map;
+  methods  : new Map;
+  rules    : new Map;
+  themes   : new Map;
+  variants : new Map;
+
+  // to register
+  rule    (obj) { this.rules   .set(obj.id, obj); }
+  method  (obj) { this.methods .set(obj.id, obj); }
+  variant (obj) { this.variants.set(obj.id, obj); }
 };
 
-export default registry;
+export default Registry;
