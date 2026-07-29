124933212675cf2e29c06aa0c5f69d84e7895904
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-29 23:52:39 +0200

    Update css.js

--- classcade/presets/css.js
+++ classcade/presets/css.js
@@ -15,56 +15,19 @@ export default cc => {
     css: value => ({ opacity: value }) 
   });
 
-};
+  cc.rule ({ 
+    id: 'block', 
+    css: value => ({ display: 'block' }) 
+  });
 
-export default cc => {
+  cc.rule ({ 
+    id: 'hidden', 
+    css: value => ({ display: 'none' }) 
+  });
 
-    cc.rule({
-
-        id:"block",
-
-        resolve(){
-
-            return {
-
-                display:"block"
-
-            };
-
-        }
-
-    });
-
-    cc.rule({
-
-        id:"inline",
-
-        resolve(){
-
-            return {
-
-                display:"inline"
-
-            };
-
-        }
-
-    });
-
-    cc.rule({
-
-        id:"hidden",
-
-        resolve(){
-
-            return {
-
-                display:"none"
-
-            };
-
-        }
-
-    });
+  cc.rule ({ 
+    id: 'inline', 
+    css: value => ({ display: 'inline' }) 
+  });
 
 };
