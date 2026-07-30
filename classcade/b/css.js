0cbaadd4c291d3242f9f2f01d43889582ded750f
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-30 18:28:35 +0200

    Create css.js

---
+++ classcade/b/css.js
@@ -0,0 +1,5 @@
+this.add({ id, kind: 'alias-prop', ref }); // definePropAlias
+this.add({ id, kind: 'alias-fn',   ref }); // defineFnAlias
+this.add({ id, kind: 'variant-media',  query    : def.media  });
+this.add({ id, kind: 'variant-prefix', selector : def.prefix });
+this.add({ id, kind: 'variant-suffix', selector : def.suffix });
