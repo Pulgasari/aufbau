1c9f15076fc25cabfcd821698ed1321b0883e430
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-30 10:07:49 +0200

    Update compiler.js

--- classcade/compiler.js
+++ classcade/compiler.js
@@ -2,19 +2,21 @@
 
 import Generator from './generator.js';
 import Injector  from './injector.js';
+import Observer  from './observer.js';
 import Parser    from './parser.js';
 import Resolver  from './resolver.js';
-import Runtime   from './runtime.js';
+import Scanner   from './scanner.js';
 
 class Compiler {
 
   constructor () {
     this.generator = new Generator;
     this.injector  = new Injector;
+    this.observer  = new Observer;
     this.parser    = new Parser;
+    this.scanner   = new Scanner ({ attributes: ['cc', 'class', 'classcade', 'className', 'data-classcade'] });    
     this.registry  = new Map;
     this.resolver  = new Resolver (this.registry);
-    this.runtime   = new Runtime  (this);
   }
 
   // registry
