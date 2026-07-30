e559f57bfb7e8695f15eaa5900e3d8ec459c0f6a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-30 09:21:38 +0200

    Update generator.js

--- classcade/generator.js
+++ classcade/generator.js
@@ -3,29 +3,23 @@
 import { arrayfied } from './utils.js';
 
 class Generator {
-
-  constructor () {}
-
   generate (nodes) {
-    return arrayfied(nodes).map(this.generateRule).filter(Boolean).join('\n\n');
+    return arrayfied(nodes).map(n => this.generateRule(n)).filter(Boolean).join('\n\n');
   }
 
   generateRule (rule) {
     if (!rule?.selector) return '';
-    const decl = Object.entries(rule.declarations ?? {};
-    const code = [];
-    
-    code.push(`${rule.selector} {`);
-    for (const [property, value] of decl)) code.push(`${property}: ${value};`);
-    code.push('}');
+    const decl  = Object.entries(rule.declarations ?? {});
+    const lines = [`${rule.selector} {`];
+    for (const [property, value] of decl) lines.push(`  ${property}: ${value};`);
+    lines.push('}');
 
-    code = code.join('\n');
-    if (rule.media)    code = `@media `    + rule.media    + `{ ${code} }`;
-    if (rule.supports) code = `@supports ` + rule.supports + `{ ${code} }`;
-    if (rule.layer)    code = `@layer `    + rule.layer}   + `{ ${code} }`;    
+    let code = lines.join('\n');
+    if (rule.media)    code = `@media ${rule.media} { ${code} }`;
+    if (rule.supports) code = `@supports ${rule.supports} { ${code} }`;
+    if (rule.layer)    code = `@layer ${rule.layer} { ${code} }`;
     return code;
   }
-
 }
 
 export default Generator;
