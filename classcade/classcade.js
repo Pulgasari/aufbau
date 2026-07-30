74948d1109c5ac75491eb6eab05099f300e40b32
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-30 04:44:34 +0200

    Update classcade.js

--- classcade/classcade.js
+++ classcade/classcade.js
@@ -2,6 +2,38 @@
 
 import Generator from './generator.js';
 import Injector  from './injector.js';
+import Resolver  from './resolver.js';
+import Runtime   from './runtime.js';
+
+class Classcade {
+
+  constructor (options={}) {
+    this.options   = options;
+    this.generator = new Generator;
+    this.injector  = new Injector;
+    this.registry  = new Map;
+    this.resolver  = new Resolver (this.registry);
+    this.runtime   = new Runtime  (this);
+  }
+
+  add (obj) { this.registry.set(obj.id, obj); return this; }
+  get (id)  { return this.registry.get(id); }
+ 
+  start () { this.runtime.start(); }
+  stop  () { this.runtime.stop();  }
+
+  use (preset) {
+    preset(this);
+    return this;
+  }
+
+}
+
+export default Classcade;
+
+/*
+import Generator from './generator.js';
+import Injector  from './injector.js';
 import Registry  from './registry.js';
 import Resolver  from './resolver.js';
 import Runtime   from './runtime.js';
@@ -30,5 +62,4 @@ class Classcade {
   }
 
 }
-
-export default Classcade;
+*/
