3089b4f9bf501997e81cd1b5a06657ebc0409b42
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-31 07:44:24 +0200

    Create aufbau-flag.js

---
+++ webcomponents/aufbau-flag.js
@@ -0,0 +1,17 @@
+import { AufbauElement } from './AufbauElement.js';
+
+export class AufbauFlag extends AufbauElement {
+  static get observedAttributes() {
+    return ['code', 'variant'];
+  }
+
+  update() {
+    const code    = (this.getAttribute('code')   || 'de').toLowerCase();
+    const variant = this.getAttribute('variant') || 'circle'; // 'circle' | 'square' | '4x3'
+
+    const iconSet = variant === 'circle' ? 'circle-flags' : 'flagpack';
+    this.innerHTML = `<aufbau-icon icon="${iconSet}:${code}"></aufbau-icon>`;
+  }
+}
+
+customElements.define('aufbau-flag', AufbauFlag);
