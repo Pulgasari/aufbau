143aaa4845bdabff27952cddcf9d09f5ac161b69
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-31 07:47:03 +0200

    Implement AufbauDropdown web component

---
+++ webcomponents/aufbau-dropdown.js
@@ -0,0 +1,26 @@
+import { AufbauElement } from './AufbauElement.js';
+
+export class AufbauDropdown extends AufbauElement {
+  static get observedAttributes() {
+    return ['label', 'open'];
+  }
+
+  update() {
+    const label = this.getAttribute('label') || 'Menu';
+    const isOpen = this.hasAttribute('open');
+
+    this.innerHTML = `
+      <details class="aufbau-dropdown-wrapper" ${isOpen ? 'open' : ''}>
+        <summary class="aufbau-dropdown-trigger">
+          <span>${label}</span>
+          <aufbau-icon icon="lucide:chevron-down"></aufbau-icon>
+        </summary>
+        <div class="aufbau-dropdown-content">
+          <slot></slot>
+        </div>
+      </details>
+    `;
+  }
+}
+
+customElements.define('aufbau-dropdown', AufbauDropdown);
