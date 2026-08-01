978c01569469c561971d7f1c5faffc8dd94860e2
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 19:06:28 +0200

    Update AufbauIcon.js

--- elements/AufbauIcon.js
+++ elements/AufbauIcon.js
@@ -15,3 +15,51 @@ export default class AufbauIcon extends AufbauElement {
 }
 
 AufbauIcon.init();
+
+/*
+
+class AufbauIcon extends HTMLElement {
+  static att r= {
+    icon : String,
+  };
+  
+  connectedCallback() {
+    this.update();
+  }
+
+  attributeChangedCallback() {
+    this.update();
+  }
+
+  update() {
+    const { icon }   = this.getAttr(); if (!icon) return;
+    const normalized = icon.replace('/', ':');
+    const url        = `https://api.iconify.design/${normalized}.svg`;
+
+    // Apply the SVG as a CSS mask
+    this.style.setProperty('--icon-url', `url("${url}")`);
+  }
+}
+
+// Global styles for the web component
+const style = document.createElement('style');
+style.textContent = `
+  aufbau-icon {
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    background-color: currentColor;
+    -webkit-mask-image: var(--icon-url);
+    mask-image: var(--icon-url);
+    -webkit-mask-repeat: no-repeat;
+    mask-repeat: no-repeat;
+    -webkit-mask-size: 100% 100%;
+    mask-size: 100% 100%;
+    vertical-align: -0.125em;
+  }
+`;
+document.head.appendChild(style);
+
+AufbauIcon.init();
+
+*/
