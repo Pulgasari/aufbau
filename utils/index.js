3da9f5a674841478e1196b12fff94abf3d25def8
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-02 00:56:59 +0200

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -27,8 +27,6 @@ sleep = (duration=1000) => new Promise( resolve => setTimeout(resolve, duration)
 arrayfied = value => isArray(value) ? value : [value],
 
 // ::: object
-
-/ object
 deepMerge = ( target, source ) => {
   if (!isPlainObject(source)) return source;
   Object.keys(source).forEach( key => {
@@ -38,3 +36,35 @@ deepMerge = ( target, source ) => {
   });
   return target;
 },
+
+// ::: events
+offEvent  = (...args) => _event(...args, 0),
+onEvent   = (...args) => _event(...args, 1),
+onceEvent = (...args) => _event(...args, 2),
+customEvent = ( target, name, value ) => {
+  let element = _el(target);
+  let event   = new CustomEvent( name, { detail: value } );
+  event.value = value;
+  element.dispatchEvent(event);
+},
+onGlobalEvent = ( selector, type, callback, options, parent=document ) => {
+  let listener = e => e.target.matches(selector) && callback(e);
+  parent.addEventListener( type, listener, options );
+},
+
+
+
+
+////////// NON-EXPORT HELPERS FOR CONSTRUCTION //////////
+
+function _event ( targets, types, listener, options, mode=1 ) {
+  let method  = (mode === 0 ? 'remove' : 'add') + 'EventListener';
+  options = mode === 2 ? { ...options, once: true } : options;
+  arrayfied(targets).forEach( 
+    target => arrayfied(types).forEach( 
+      type => _el(target)?.[method](type, listener, options) 
+    )
+  );
+  if (mode === 1) return () => offEvent( targets, types, listener);
+};
+
