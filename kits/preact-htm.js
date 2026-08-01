3143839f0890c0d00e3ab051def1454de9f3b470
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-02 00:34:12 +0200

    Update preact-htm.js

--- kits/preact-htm.js
+++ kits/preact-htm.js
@@ -3,14 +3,7 @@
 // :::::: IMPORT ::::::::::::::::::::::::::::::::::::::::::::::::
 
 // ::: AUFBAU
-import      aufbauCache         from '@aufbau/cache';
-import      aufbauElements      from '@aufbau/elements';
-import      aufbauImport        from '@aufbau/import';
-import * as aufbauPluginsClient from '@aufbau/plugins/client';
-import * as aufbauPluginsWorker from '@aufbau/plugins/worker';
-import * as aufbauShaders       from '@aufbau/shaders';
-import * as aufbauStylesheet    from '@aufbau/stylesheet';
-import * as aufbauUtils         from '@aufbau/utils';
+import aufbau  from '@aufbau/kits/aufbau.js';
 
 // ::: HTM
 import htm from 'htm'; 
@@ -20,7 +13,7 @@ import * as preactCore    from 'preact';
 import * as preactHooks   from 'preact/hooks';
 import * as preactSignals from '@preact/signals';
 
-// :::::: BUILD :::::::::::::::::::::::::::::::::::::::::::::::::
+// :::::: BUNDLE ::::::::::::::::::::::::::::::::::::::::::::::::
 
 const html = htm.bind(h);
 
@@ -28,53 +21,16 @@ const preact = {
   ...preactCore,
   ...preactHooks,
   ...preactSignals,
-};c
-
-const aufbau = {
-  // AUFBAU
-  config, configs, 
-  init, interceptFetch,
-
-  // aufbau-dirty (needs to be moved)
-  define, update, updateDataset, updateProperty,
-
-  // aufbau-packages
-  cache      : aufbauCache,
-  elements   : aufbauElements,
-  import     : aufbauImport,
-  shaders    : aufbauShaders,
-  stylesheet : aufbauStylesheet,
-  utils      : aufbauUtils,
-
-  // aufbau adapters
-  plugins = {
-    client : aufbauPluginsClient,
-    worker : aufbauPluginsWorker
-  },
-
-  // Preact + HTM
-  html, preact,
 };
 
-export function init () {
-  if (typeof window !== 'undefined') {
-    aufbauPluginsClient.observeStylesheets();
-  }
-}
-
-
-// Combined master fetch handler for Service Workers.
-// Checks all registered Aufbau plugins in sequence.
-export async function interceptFetch (event) {
-  // 1. Check stylesheet plugin
-  const stylesheetResponse = await aufbauPluginsWorker.interceptFetchStylesheet(event);
-  if (stylesheetResponse) return stylesheetResponse;
-  return null;
-}
+aufbau.htm    = htm;
+aufbau.html   = html;
+aufbau.preact = preact;
 
 // :::::: EXPORT ::::::::::::::::::::::::::::::::::::::::::::::::
 
 export aufbau;
+export htm;
 export html;
 export preact;
 
