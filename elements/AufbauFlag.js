df33dba8185241ee279412c28caa9d4db481f7fb
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 15:39:13 +0200

    Update AufbauFlag.js

--- elements/AufbauFlag.js
+++ elements/AufbauFlag.js
@@ -3,14 +3,16 @@
 import AufbauElement from './AufbauElement.js';
 
 export default class AufbauFlag extends AufbauElement {
-  static attr = ['code', 'variant'];
+  static attr = {
+    code    : 'de',
+    variant : 'circle', // 'circle' | 'square' | '4x3'
+  };
 
   update() {
-    const code    = (this.getAttr('code')   || 'de').toLowerCase();
-    //const variant = this.getAttr('variant') || 'circle'; // 'circle' | 'square' | '4x3'
+    const code    = (this.getAttr('code')).toLowerCase();
     const variant = this.getConfig('variant', 'flag-variant', 'circle');
-    
     const iconSet = variant === 'circle' ? 'circle-flags' : 'flagpack';
+    
     this.innerHTML = `<aufbau-icon icon="${iconSet}:${code}"></aufbau-icon>`;
   }
 }
