0691153b928a1f99724b0097ffc33884a7db5bd9
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-02 00:45:25 +0200

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,25 @@
+// @aufbau/utils
 
 
-/**
- * Convert heading text into a URL-friendly slug.
- */
 export function slugify (text) {
   return text
     .toLowerCase()
     .replace(/[^\w\s-]/g, '')
     .replace(/\s+/g, '-');
 }
+
+
+
+
+export const // timing
+debounce  = ( fn, delay=100 ) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn(...args), delay);
+  }
+},
+sleep = (duration=1000) => new Promise( resolve => setTimeout(resolve, duration) ),     
+
+  // array
+arrayfied = value => isArray(value) ? value : [value],
