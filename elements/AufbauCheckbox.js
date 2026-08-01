4b5b61ad8b8fe998bf8d43a8e803583ddfaa9a6b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 12:57:46 +0200

    Update AufbauCheckbox.js

--- elements/AufbauCheckbox.js
+++ elements/AufbauCheckbox.js
@@ -2,50 +2,38 @@
 
 import AufbauElement from './AufbauElement.js';
 
-export class AufbauCheckbox extends AufbauElement {
-  static get observedAttributes() {
+export default class AufbauCheckbox extends AufbauElement {
+  static get observedAttributes () {
     return ['checked', 'disabled', 'label', 'value', 'indeterminate'];
   }
 
-  onMount() {
-    this.addEventListener('click', this.toggle.bind(this));
+  onMount () {
+    this.on('click', () => this.toggle());
   }
 
-  toggle() {
+  toggle () {
     if (this.hasAttribute('disabled')) return;
-    const isChecked = this.hasAttribute('checked');
-    
-    if (isChecked) {
-      this.removeAttribute('checked');
-    } else {
-      this.setAttribute('checked', '');
-    }
 
-    this.emit('aufbau-checkbox', { 
-      checked: !isChecked, 
-      value: this.getAttribute('value') || 'on' 
-    });
+    const checked = !this.hasAttribute('checked');
+    this.setAttr({ checked });
+    this.emit('aufbau-checkbox', { checked, value: this.getAttr('value', String, 'on') });
   }
 
-  update() {
-    const isChecked = this.hasAttribute('checked');
-    const isDisabled = this.hasAttribute('disabled');
-    const isIndeterminate = this.hasAttribute('indeterminate');
-    const label = this.getAttribute('label') || '';
-
-    let stateAttr = isChecked ? 'true' : 'false';
-    if (isIndeterminate) stateAttr = 'mixed';
+  update () {
+    const label = this.getAttr('label', String, '');
+    const { checked, disabled, indeterminate } = this.getAttr(Boolean);
+    const state = indeterminate ? 'mixed' : String(checked);
 
     this.innerHTML = `
-      <label class="aufbau-checkbox-wrapper ${isDisabled ? 'is-disabled' : ''}">
-        <button 
-          type="button" 
-          role="checkbox" 
-          aria-checked="${stateAttr}" 
-          class="checkbox-box ${isChecked ? 'is-checked' : ''} ${isIndeterminate ? 'is-indeterminate' : ''}"
-          ${isDisabled ? 'disabled' : ''}
+      <label class="aufbau-checkbox-wrapper ${disabled ? 'is-disabled' : ''}">
+        <button
+          type="button"
+          role="checkbox"
+          aria-checked="${state}"
+          class="checkbox-box ${checked ? 'is-checked' : ''} ${indeterminate ? 'is-indeterminate' : ''}"
+          ${disabled ? 'disabled' : ''}
         >
-          <aufbau-icon icon="${isIndeterminate ? 'lucide:minus' : 'lucide:check'}"></aufbau-icon>
+          <aufbau-icon icon="${indeterminate ? 'lucide:minus' : 'lucide:check'}"></aufbau-icon>
         </button>
         ${label ? `<span class="checkbox-label">${label}</span>` : ''}
       </label>
@@ -53,5 +41,4 @@ export class AufbauCheckbox extends AufbauElement {
   }
 }
 
-customElements.define('aufbau-checkbox', AufbauCheckbox);
-export default AufbauCheckbox;
+AufbauCheckbox.init();
