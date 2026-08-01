dd9cd1b98fb372157dd05193b48b884882dad309
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 04:07:43 +0200

    Update index.js

--- domina/index.js
+++ domina/index.js
@@ -1,18 +1,82 @@
 // @domina
 
+import * from './utils.js';
+
+/*
 import createElement from './createElement.js';
 import getElements   from './getElements.js';
 import insertElement from './insertElement.js';
 import resolveEDO    from './resolveEDO.js';
+*/
 
+export const 
 
-export const create = (edo = {}, ...children) => {
+// ============ GET ============
+
+getElementById        = id          => document.getElementById(selector),
+getElement            = (spec, ctx) => _doc(context).querySelector(_slct(selector)),
+getElements           = (spec, ctx) => [..._doc(context).querySelectorAll(_slct(selector))],     
+getElementsByDataAttr = (key,  ctx) => getElements(`[data-${key}]`, parent),
+getElementsByDataKey  = (key,  ctx) => getElements(`[data-key="${key}"]`, parent),
+
+// ============ CREATE ============
+
+create = (edo = {}, ...children) => {
   const { tag, tagName, ...props } = edo;
   const what = tag || tagName || 'div';
   return createElement (what, props, ...children);
+},
+  
+createElement = (tag = 'div', props = {}, ...children) => {
+  const el = document.createElement(tag);
+  updateElement(el, props, ...children);
+  return el;
 };
 
-export const get = (edoOrSelector, context) => getElements (edoOrSelector, context);
 
-// accepts Node, HTML string, selector, EDO or array thereof
-export const insert = (sth, target = 'body', mode = 'append') => insertElement (sth, target, mode);      
+export default {
+  createElement,
+  getElement,
+  getElements,
+  insertElement,
+  
+  element  : getElement,
+  elements : getElements,
+}
+
+
+/*
+
+aufbau.get({ id: 'app-header' })
+aufbau.get('#app-header')
+
+aufbau.dom.get({ id: 'app-header' })
+aufbau.dom.get('#app-header'
+
+domina.get({ id: 'app-header' })
+domina.get('#app-header')
+
+dom.get({ id: 'app-header' })
+dom.get('#app-header')
+
+dom.element({ id: 'app-header' })
+dom.element('#app-header')
+
+dom.elements({ dataKey: 'app-header' })
+dom.elements('#app-header')
+
+aufbau.dom.element({ id: 'app-header' })
+aufbau.dom.element('#app-header')
+
+*/
+
+
+
+
+
+
+
+
+
+
+
