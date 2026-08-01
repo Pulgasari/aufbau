b90536ae863f0f7eed2b89a1f0f7bb233a0c5005
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 18:36:34 +0200

    Create index.d.ts

---
+++ elements/index.d.ts
@@ -0,0 +1,14 @@
+/*
+import type { HTMLAttributes } from "react";
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "aufbau-icon": HTMLAttributes<HTMLElement> & {
+        name?: string;
+        size?: string | number;
+      };
+    }
+  }
+}
+*/
