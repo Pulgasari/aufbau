a28321067443d2708c7b2a193c34f9dba9a6d688
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-02 01:24:21 +0200

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -29,6 +29,52 @@ sleep = (duration=1000) => new Promise( resolve => setTimeout(resolve, duration)
 
 // array
 arrayfied = value => isArray(value) ? value : [value],
+filterList = ( input, config ) => {
+  
+  // Define Input
+  let list = isObject(input) ? Object.keys(input) : Array.from( input || [] );
+  
+  // Predicate
+  let getPredicates = ( conf, negate=false ) => {
+    let preds = [];
+    let add   = (key, fn) => !isNullish(conf[key]) && preds.push( negate ? (item => !fn(item)) : fn );
+    
+    // regular filters
+    add( 'includes'   , item => item.includes(conf.includes));
+    add( 'endsWith'   , item => item.endsWith(conf.endsWith));
+    add( 'startsWith' , item => item.startsWith(conf.startsWith));
+    add( 'length'     , item => item.length === conf.length);
+    add( 'maxLength'  , item => item.length <= conf.maxLength);
+    add( 'minLength'  , item => item.length >= conf.minLength);
+    add( 'regex'      , item => conf.regex.test(item));
+    
+    // custom filters
+    if (isArray(conf.filter)) conf.filter.forEach( f => preds.push(negate ? (item => !f(item)) : f ));
+    
+    // not (negation)
+    if (conf.not) preds.push( ...getPredicates(conf.not, !negate) );
+    
+    //
+    return preds;
+  };
+  
+  // Cache Predicates
+  let cachedPredicates = getPredicates(config);
+  
+  // Process
+  list = list.filter( 
+    item => cachedPredicates.every( p => p(item) ) 
+  );
+  
+  // Post-Process
+  if (config.toLowerCase)     list = list.map( x => x.toLowerCase() );
+  if (config.toUpperCase)     list = list.map( x => x.toUpperCase() );
+  if (isArray(config.output)) config.output.forEach( f => list = list.map(f) );
+  if (config.sort)            list = isFn(config.sort) ? list.sort(config.sort) : list.sort();
+  
+  // Final Output
+  return list;
+},
 
 // ::: object
 deepMerge = ( target, source ) => {
