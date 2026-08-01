3a39ae0d223cc66a9776834c73b1a709de06cbf8
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 06:08:20 +0200

    Update aufbau-index.js

--- elements/aufbau-index.js
+++ elements/aufbau-index.js
@@ -1,6 +1,6 @@
-// <aufbau-index> | <aufbau-item>
+// <aufbau-index>
 
-class AufbauIndex extends HTMLElement {
+export class AufbauIndex extends HTMLElement {
   static get observedAttributes() {
     return ['item-size', 'item-shape', 'item-look', 'gap'];
   }
@@ -38,32 +38,4 @@ class AufbauIndex extends HTMLElement {
   }
 }
 
-class AufbauItem extends HTMLElement {
-  static get observedAttributes() {
-    return ['shape', 'look'];
-  }
-
-  connectedCallback() {
-    this.updateProperties();
-  }
-
-  attributeChangedCallback() {
-    this.updateProperties();
-  }
-
-  updateProperties() {
-    const look = parseLook(this.getAttribute('look'));
-    const shape = this.getAttribute('shape') || look.shape;
-
-    if (shape) {
-      const shapes = {
-        circle: '50%',
-        square: '0px',
-        rounded: '12px',
-        squircle: '24% / 50%'
-      };
-      this.style.setProperty('--aufbau-current-shape', shapes[shape] || shape);
-    }
-  }
-}
-
+export default AufbauIndex;
