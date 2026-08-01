ed9f2a48f431768c59dc60c1e2048b574b3ac1c5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 11:47:00 +0200

    Update AufbauIcon.js

--- elements/AufbauIcon.js
+++ elements/AufbauIcon.js
@@ -3,18 +3,16 @@
 import AufbauElement from './AufbauElement.js';
 
 export class AufbauIcon extends AufbauElement {
-  static get observedAttributes() {
+  static get observedAttributes () {
     return ['icon'];
   }
 
-  update() {
-    const icon = this.getAttribute('icon'); if (!icon) return;
-    const normalized = icon.replace('/', ':');
-    const url = `https://api.iconify.design/${normalized}.svg`;
-
+  update () {
+    const icon = this.attr('icon'); if (!icon) return;
+    const url  = `https://api.iconify.design/${icon.replace('/', ':')}.svg`;
     this.style.setProperty('--icon-url', `url("${url}")`);
   }
 }
 
-customElements.define('aufbau-icon', AufbauIcon);
+if (!customElements.get('aufbau-icon')) customElements.define('aufbau-icon', AufbauIcon);
 export default AufbauIcon;
