634bfbac8b2213962105179e53d6f34fb3191c25
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 19:26:54 +0200

    Create index.js

---
+++ elements/core/index.js
@@ -0,0 +1,7 @@
+// @aufbau/elements/core
+
+import AufbauConfig from './AufbauConfig.js';
+import AufbauCore   from './AufbauCore.js';
+
+export class AufbauDatalistElement extends AufbauCore (HTMLDataListElement) {}    
+export class AufbauElement         extends AufbauCore (HTMLElement)         {}
