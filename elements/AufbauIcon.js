321659766b10836cd17d62858b82b19d43f2fa25
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-01 13:34:25 +0200

    Update AufbauIcon.js

--- elements/AufbauIcon.js
+++ elements/AufbauIcon.js
@@ -2,17 +2,14 @@
 
 import AufbauElement from './AufbauElement.js';
 
-export class AufbauIcon extends AufbauElement {
-  static get observedAttributes () {
-    return ['icon'];
-  }
+export default class AufbauIcon extends AufbauElement {
+  static attr = ['icon'];
 
   update () {
-    const icon = this.attr('icon'); if (!icon) return;
+    const icon = this.getAttr('icon'); if (!icon) return;
     const url  = `https://api.iconify.design/${icon.replace('/', ':')}.svg`;
     this.style.setProperty('--icon-url', `url("${url}")`);
   }
 }
 
-if (!customElements.get('aufbau-icon')) customElements.define('aufbau-icon', AufbauIcon);
-export default AufbauIcon;
+AufbauIcon.init();
