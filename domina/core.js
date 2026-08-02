f2588cb8897eecc6e7290832789e75de29d275aa
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-02 22:03:39 +0200

    Update core.js

--- domina/core.js
+++ domina/core.js
@@ -2,10 +2,18 @@
 
 import { isElementish, isObject } from './utils.js';
 
+export const // ============ GET ============
+getElementById        = (id,   ctx) =>     _doc(ctx).getElementById(id),
+getElement            = (spec, ctx) =>     _doc(ctx).querySelector   (_slct(spec)),
+getElements           = (spec, ctx) => [..._doc(ctx).querySelectorAll(_slct(spec))],     
+getElementsByDataAttr = (key,  ctx) => getElements(`[data-${key}]`,       ctx),
+getElementsByDataKey  = (key,  ctx) => getElements(`[data-key="${key}"]`, ctx);
+
+
 export const
 _doc  = sth => sth ? _el(sth) : document,
-_el   = sth => isElementish(sth) ? sth : getElement(_slct(sth)),
 _root = sth => sth ? _el(sth) : $root,
+_el   = sth => isElementish(sth) ? sth : getElement(_slct(sth)),
 
 /**
  * Converts a selector string or EDO into a CSS selector.
