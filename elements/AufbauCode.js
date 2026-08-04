a265b6f952477d83aca62e3668922c87c739526d
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-04 16:27:39 +0200

    Update AufbauCode.js

--- elements/AufbauCode.js
+++ elements/AufbauCode.js
@@ -1,6 +1,6 @@
 // <aufbau-code>
 
-import { AufbauElement } from './core/AufbauCore.js';
+import { AufbauElement } from './core/index.js';
 
 // lazy loader singleton for highlight.js
 let hljsPromise = null;
