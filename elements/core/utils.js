30c7576cb6e5916f1e051c51577ee957b81478f8
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-05 01:45:05 +0200

    Update utils.js

--- elements/core/utils.js
+++ elements/core/utils.js
@@ -1,1 +1,48 @@
+// @aufbau/elements/core/utils.js
+
 export const toKebabCase = (str) => str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();     
+
+export const decorateElement = (el) => {
+  if (!el || el._aufbauDecorated) return el;
+
+  Object.defineProperties(el, {
+    _aufbauDecorated: { value: true, configurable: true },
+    on: {
+      value (...args) {
+        this.addEventListener(...args);
+        return () => this.removeEventListener(...args);
+      },
+      writable: true, configurable: true
+    },
+    off: {
+      value (...args) {
+        this.removeEventListener(...args);
+        return this;
+      },
+      writable: true, configurable: true
+    }
+  });
+
+  return el;
+};
+
+export const decorateArray = (arr) => {
+  Object.defineProperties(arr, {
+    on: {
+      value (...args) {
+        const unsubs = this.map(el => el.on?.(...args) ?? (() => {}));
+        return () => unsubs.forEach(unsub => unsub());
+      },
+      writable: true, configurable: true
+    },
+    off: {
+      value (...args) {
+        this.forEach(el => el.off?.(...args));
+        return this;
+      },
+      writable: true, configurable: true
+    }
+  });
+
+  return arr;
+};
