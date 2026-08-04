6d7098ceda88032f8ecc2226e725de7da3beeba4
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-04 16:30:51 +0200

    Update AufbauSwitch.js

--- elements/AufbauSwitch.js
+++ elements/AufbauSwitch.js
@@ -1,6 +1,6 @@
 // <aufbau-switch>
 
-import { AufbauElement } from './AufbauElement.js';
+import { AufbauElement } from './core/index.js';
 
 export default class AufbauSwitch extends AufbauElement {
   static attr = {
