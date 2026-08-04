288b2748101c94d2da8e865617a45bdba0b7d3de
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-04 16:18:58 +0200

    Update AufbauAudio.js

--- elements/AufbauAudio.js
+++ elements/AufbauAudio.js
@@ -1,6 +1,6 @@
 // <aufbau-audio>
 
-import { AufbauElement } from './core/AufbauElement.js';
+import { AufbauElement } from './core/AufbauCore.js';
 
 const formatTime = (seconds) => {
   if (Number.isNaN(seconds)) return '0:00';
