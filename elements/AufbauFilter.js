0c6281f14030a7ee9ecb5c67985483e1859d5165
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-04 16:28:42 +0200

    Update AufbauFilter.js

--- elements/AufbauFilter.js
+++ elements/AufbauFilter.js
@@ -1,6 +1,6 @@
 // <aufbau-filter>
 
-import { AufbauElement } from './core/AufbauCore.js';
+import { AufbauElement } from './core/index.js';
 
 export default class AufbauFilter extends AufbauElement {
   static attr = {
