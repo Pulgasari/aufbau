37f4e7ef215b89011dfcae2273767564df7933f2
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-04 16:20:59 +0200

    Update AufbauCombobox.js

--- elements/AufbauCombobox.js
+++ elements/AufbauCombobox.js
@@ -1,7 +1,7 @@
 // <aufbau-combobox>
 
 import { AufbauElement } from './core/AufbauCore.js';
-import importFile        from '@aufbau/import';
+import { importFile }    from '@aufbau/import';
 
 export default class AufbauCombobox extends AufbauElement {
   
