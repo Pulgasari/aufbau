c3234a8433e8912f4ebe623633873a102765d796
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-05 22:01:20 +0200

    Update string.js

--- utils/fp/string.js
+++ utils/fp/string.js
@@ -32,15 +32,16 @@ trimStart = value => value.trimStart   ();
 
 // :::::: CONFIGURED TRANSFORMS
 
-export const padEnd     = (length, filler = ' ') => (value) => value.padEnd(length, filler);
-export const padStart   = (length, filler = ' ') => (value) => value.padStart(length, filler);
-export const prefix     = (text) => (value) => ensurePrefix(value, text);
-export const replace    = (search, replacement) => (value) => value.replace(search, replacement);
-export const replaceAll = (search, replacement) => (value) => value.replaceAll(search, replacement);
-export const split      = (separator) => (value) => value.split(separator);
-export const suffix     = (text) => (value) => ensureSuffix(value, text);
-export const template   = (values) => (value) => templateOf(value, values);
-export const truncate   = (length, ending) => (value) => truncateOf(value, length, ending);
+export const
+padEnd     = (length, filler = ' ') => (value) => value.padEnd(length, filler),
+padStart   = (length, filler = ' ') => (value) => value.padStart(length, filler),
+prefix     = (text)                 => (value) => ensurePrefix(value, text),
+replace    = (search, replacement)  => (value) => value.replace(search, replacement),
+replaceAll = (search, replacement)  => (value) => value.replaceAll(search, replacement),    
+split      = (separator)            => (value) => value.split(separator),
+suffix     = (text)                 => (value) => ensureSuffix(value, text),
+template   = (values)               => (value) => templateOf(value, values),
+truncate   = (length, ending)       => (value) => truncateOf(value, length, ending);
 
 // :::::: QUERY
 
