f4276aa01b6f267682d7e013703dbc78bc9585c8
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-05 14:48:30 +0200

    Update AufbauConfig.js

--- elements/core/AufbauConfig.js
+++ elements/core/AufbauConfig.js
@@ -2,7 +2,7 @@
 // <aufbau-config>
 // central store for global configuration values, read via AufbauCore#getConfig()
 
-import { CanonicalMap, createLogger, emit, isPlainObject, isString, onEvent, toJson, toKebabCase } from '@aufbau/utils';
+import { CanonicalMap, createLogger, emitEvent, isPlainObject, isString, onEvent, toJson, toKebabCase } from '@aufbau/utils';
 
 const log = createLogger('aufbau-config');
 
