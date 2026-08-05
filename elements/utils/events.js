d5d49bf71c5ded1a36749eb1e3458ae12e2d7406
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-05 02:53:55 +0200

    Create events.js

---
+++ elements/utils/events.js
@@ -0,0 +1,21 @@
+// @aufbau/elements/utils/events.js
+
+//import { on, off } from '@aufbau/dom/events';
+//import { on, off } from '@domina/events';
+
+export const onEvent = (target, type, listener, options) => {
+  target.addEventListener(type, listener, options);
+  return () => target.removeEventListener(type, listener, options);
+};
+
+export const offEvent = (target, type, listener, options) => {
+  target.removeEventListener(type, listener, options);
+  return target;
+};
+
+export const emitEvent (target, eventName, detail = {}, options = {}) {
+  return target.dispatchEvent (new CustomEvent (eventName, {
+    bubbles: true, composed: true, detail, ...options
+  }));
+}
+
