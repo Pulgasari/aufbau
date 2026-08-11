14ff60fd0be0f77f456eb1934896ec6f2bd487ae
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-11 18:29:01 +0200

    Update aufbau.js

--- kits/aufbau.js
+++ kits/aufbau.js
@@ -125,6 +125,25 @@ export async function interceptFetch (event) {
   return null;
 }
 
+/*
+// poo/playground/sw.js
+
+import { interceptFetch } from '@aufbau/kit';
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    (async () => {
+      // Intercept Aufbau stylesheets and assets
+      const aufbauResponse = await interceptFetch(event);
+      if (aufbauResponse) return aufbauResponse;
+
+      // Fallback to network fetch
+      return fetch(event.request);
+    })()
+  );
+});
+*/
+
 // :::::: BUNDLE :::::::::::::::::::::::::::::::::::::::::::::::::
 
 const aufbau = {
