25c360f449a902146f51d795eb65ffdf7e312548
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-16 14:18:08 +0200

    Create RuleBuilder.js

---
+++ stylescript/RuleBuilder.js
@@ -0,0 +1,31 @@
+// classes/RuleBuilder.js
+
+class RuleBuilder {
+  constructor() {
+    this.declarations = [];
+  }
+
+  set(prop, value) {
+    if (typeof prop === 'object') {
+      for (const [k, v] of Object.entries(prop)) {
+        this.set(k, v);
+      }
+    } else {
+      const cssProp = prop.replace(/[A-Z]/g, m => `-${m.toLowerCase()}`);
+      this.declarations.push(`  ${cssProp}: ${value};`);
+    }
+    return this;
+  }
+
+  use(trait) {
+    return this.set(trait);
+  }
+
+  flex(direction = 'row', align = 'stretch') {
+    return this.set({ display: 'flex', flexDirection: direction, alignItems: align });
+  }
+
+  getStyles() {
+    return this.declarations;
+  }
+}
