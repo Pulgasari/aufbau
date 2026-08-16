a3c2ade34a34453fa52b13b8d3d105d9659c43dc
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-16 14:21:53 +0200

    Update index.js

--- stylescript/index.js
+++ stylescript/index.js
@@ -85,25 +85,7 @@ function parseObjectOrArray (styles, parentSelector = '') {
   return cssString;
 }
 
-/**
- * Chainable Style Builder Engine.
- */
-class StyleBuilder {
-  constructor() {
-    this.rules = [];
-  }
 
-  rule(selector, callback) {
-    const subBuilder = new RuleBuilder();
-    callback(subBuilder);
-    this.rules.push({ selector, styles: subBuilder.getStyles() });
-    return this;
-  }
-
-  toCSS() {
-    return this.rules.map(r => `${r.selector} {\n${r.styles.join('\n')}\n}`).join('\n');
-  }
-}
 
 
 
