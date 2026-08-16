58ab3e2f37d67ea93d267b97cb21ba43eb5cc978
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-16 15:33:04 +0200

    Update index.js

--- stylescript/classes/index.js
+++ stylescript/classes/index.js
@@ -1,6 +1,6 @@
 // classes/index.js
 
-export * from './compileStyleInput.js';
-export * from './defineTokens.js';
 export * from './RuleBuilder.js';
 export * from './StyleBuilder.js';
+export * from './StyleSheet.js';
+export * from './TraitStore.js';
