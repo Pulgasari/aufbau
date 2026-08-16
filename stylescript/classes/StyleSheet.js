95f40b0548b6b97905e70de7973b0922df2e6a54
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-16 14:23:28 +0200

    Create StyleSheet.js

---
+++ stylescript/classes/StyleSheet.js
@@ -0,0 +1,27 @@
+// classes/StyleSheet.js
+
+/**
+ * Main StyleScript Sheet Factory using domina under the hood.
+ */
+export class StyleSheet {
+  constructor(name, inputOptions = {}) {
+    this.name = name;
+    this.rawCSS = '';
+    this.sheetInstance = null;
+  }
+
+  define(input) {
+    this.rawCSS += compileStyleInput(input) + '\n';
+    return this;
+  }
+
+  /**
+   * Builds and adopts stylesheet using domina core methods.
+   */
+  adopt(target = document) {
+    // Utilize domina's native createStylesheet & adoptStylesheet primitives
+    this.sheetInstance = createStylesheet(this.rawCSS, { id: this.name });
+    adoptStylesheet(this.sheetInstance, target);
+    return this.sheetInstance;
+  }
+}
