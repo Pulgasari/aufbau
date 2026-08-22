855f837b216b9b75753a6988f9319f5ab5f899eb
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-22 14:23:05 +0200

    Update init.js

--- runtime/init.js
+++ runtime/init.js
@@ -3,33 +3,36 @@
 import * as client   from './client.js';
 import * as elements from '@aufbau/elements';
 import * as utils    from '@aufbau/js';
+import configs       from './configs.js';
 
-import configs from './configs.js';
-
-const { deepMerge, isPlainObject } = aufbauUtils;
-console.log('[runtime] configs:', configs);
-
-const RESERVED_ELEMENT_KEYS = new Set(['mode']);
+const { deepMerge, isPlainObject } = utils;
+const BAD_KEYS  = new Set(['mode']);
 const normalizeElements = value => value ?? null;
 
-// :::::: CONFIG ::::::::::::::::::::::::::::::::::::::::::::::::
+let isBooted = false;
+
+// :::::: DEBUG
+
+console.log('[runtime] configs:', configs);
+
+// :::::: CONFIG
 
 // pushes element config into the AufbauConfigStore as the lowest layer
 function syncElementConfig () {
   const entries = {};
   for (const [key, value] of Object.entries(configs.elements)) {
-    if (!RESERVED_ELEMENT_KEYS.has(key)) entries[key] = value;
+    if (!BAD_KEYS.has(key)) entries[key] = value;
   }
-  aufbauElements.setConfig(entries, { layer: 'defaults' });
+  elements.setConfig(entries, { layer: 'defaults' });
 }
 
 function config (options = {}) {
   const { elements, splash, stylesheet, ...rest } = options;
 
   const normalized = normalizeElements(elements);
-  if (normalized)               deepMerge(configs.elements, normalized);
-  if (splash)                   deepMerge(configs.splash, splash);
-  if (stylesheet !== undefined) configs.stylesheet = stylesheet;
+  if (normalized) deepMerge(configs.elements, normalized);
+  if (splash)     deepMerge(configs.splash, splash);
+  if (stylesheet) configs.stylesheet = stylesheet;
 
   deepMerge(configs.elements, rest);
 
@@ -37,25 +40,26 @@ function config (options = {}) {
   return configs;
 }
 
-// :::::: RUNTIME ::::::::::::::::::::::::::::::::::::::::::::::::
+// :::::: INIT
 
-let initialized = false;
+
 
 async function initElements ({ mode = 'auto' }) {
        if (mode === 'auto')      elements.autoloader();
   else if (mode === 'all') await elements.registerAll();
 }
 
+const initStylesheet = (bool) => bool && client.observeStylesheets();
+
 // boots the aufbau runtime in the browser
 async function init (options = {}) {
   config(options);
 
-  if (typeof window === 'undefined' || initialized) return aufbau;
-  initialized = true;
-  
-  if (configs.stylesheet) aufbauClient.observeStylesheets();
-
-  initElements(configs.elements);
+  if (typeof window !== 'undefined' && !isBooted) {
+    initElements   (configs.elements);
+    initStylesheet (configs.stylesheet);
+    isBooted = true;
+  }
   
   return aufbau;
 }
