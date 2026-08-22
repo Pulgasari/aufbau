35043bdda34b33dd32d9268ed5a4be7452cf83db
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-22 15:53:52 +0200

    Update configs.js

--- runtime/configs.js
+++ runtime/configs.js
@@ -1,17 +1,25 @@
-const x = {
-  // 'auto' = lazy autoloader, elements are fetched when they appear in the dom
-  // 'all'  = eagerly register every element up front
-  // false  = do not touch @aufbau/elements at all
-  elements : { mode: 'auto' },
+export default {
+  
+  elements : { 
+    mode: 'auto', // 'all' | 'auto' | false
+  },
 
   // observe <link>/<style> and transform aufbau stylesheets client-side
   stylesheet : true,
 
   // data
-  layouts : ['mobile'],
-  looks   : ['flat', 'rounded'],
-  skins   : ['monochrome'],
-  themes  : ['classic', 'oled', 'rainbow', 'zombie'],
-};
+  data : {
+    layouts : ['mobile'],
+    looks   : ['flat', 'rounded'],
+    skins   : ['monochrome'],
+    themes  : ['classic', 'oled', 'rainbow', 'zombie'],
+  },
 
-export default x;
+  // appearance
+  css  : ['aufbau'],
+  font : ['Manrope'],
+
+  look  : 'flat',
+  skin  : 'monochrome',
+  theme : 'zombie',
+};
