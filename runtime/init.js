7021915a59878e9f03ee8952b60e19bc9d35f90c
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-22 13:11:09 +0200

    Update init.js

--- runtime/init.js
+++ runtime/init.js
@@ -1,8 +1,8 @@
 // init.js
 
-import * as aufbauElements from '@aufbau/elements';
-import * as aufbauUtils    from '@aufbau/js';
-import * as aufbauClient   from './client.js';
+import * as client   from './client.js';
+import * as elements from '@aufbau/elements';
+import * as utils    from '@aufbau/js';
 
 import configs from './configs.js';
 
@@ -31,8 +31,6 @@ function config (options = {}) {
   if (splash)                   deepMerge(configs.splash, splash);
   if (stylesheet !== undefined) configs.stylesheet = stylesheet;
 
-  // bare keys keep going to the element config, which is how this has always
-  // been called: config({ mode: 'all' }), config({ 'toast-duration': 2000 })
   deepMerge(configs.elements, rest);
 
   syncElementConfig(); // also runs on calls after boot, so config() stays live
@@ -43,6 +41,11 @@ function config (options = {}) {
 
 let initialized = false;
 
+async function initElements ({ mode = 'auto' }) {
+       if (mode === 'auto')      elements.autoloader();
+  else if (mode === 'all') await elements.registerAll();
+}
+
 // boots the aufbau runtime in the browser
 async function init (options = {}) {
   config(options);
@@ -52,11 +55,8 @@ async function init (options = {}) {
   
   if (configs.stylesheet) aufbauClient.observeStylesheets();
 
-       if (configs.elements.mode === 'auto')      aufbauElements.autoloader();
-  else if (configs.elements.mode === 'all') await aufbauElements.registerAll();
-
-  //if (configs.splash.fonts) aufbauUtils.gate('fonts', domina.fontsReady);
-
+  initElements(configs.elements);
+  
   return aufbau;
 }
 
