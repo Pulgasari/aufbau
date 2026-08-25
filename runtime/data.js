2c6c913732cfa7263b251c98b33012626873dd8a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-25 02:14:55 +0200

    Create data.js

---
+++ runtime/data.js
@@ -0,0 +1,20 @@
+// @aufbau/runtime/data.js
+
+/*
+filters
+patterns
+stylescript
+webfonts
+*/
+
+const css = {
+  layouts : ['mobile'],
+  looks   : ['flat', 'rounded'],
+  skins   : ['monochrome'],
+  themes  : ['classic', 'oled', 'rainbow', 'zombie'],
+};
+
+
+
+export         { css };
+export default { css };
