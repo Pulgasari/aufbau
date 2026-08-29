e2d15b5a7d7574d4915fc49f35e87951fb857e58
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-29 18:07:02 +0200

    Update shared.js

--- signals/shared.js
+++ signals/shared.js
@@ -9,11 +9,17 @@ isPlainObject = value => value !== null && typeof value === 'object' && !Array.i
 isPromise     = value => value !== null && typeof value?.then === 'function';
 //isNode        = value => value !== null && typeof value === 'object' && _meta.has(value);
 
+export function toEntries (sth) {
+  return Array.isArray(sth) ? sth : Object.entries(sth);
+}
+
 // ::: by vendors 
 // (will later be copied into this package directly)
 export { isFn, isString } from '@pulgasari/is';
 export { default as arr } from '@pulgasari/arr';
 export { default as obj } from '@pulgasari/obj';
+
+export * from '@preact/signals';
 
 /*
 let isObject      = value => value !== null && typeof value === 'object';
@@ -21,3 +27,8 @@ let isPlainObject = value => isObject(value) && (value.constructor === Object ||
 let isPromise     = value => isObject(value) && typeof value.then === 'function';
 let isNode        = value => isObject(value) && _meta.has(value);
 */
+
+
+
+
+
