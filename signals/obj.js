dce2df67a27b946c52bef029011d61b9f7182ca6
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-29 07:59:34 +0200

    Update obj.js

--- signals/obj.js
+++ signals/obj.js
@@ -10,22 +10,57 @@ obj.resolvePath = (object, dotKey) => {
 
   return { target, key, value };
 };
+
+obj.assign = (target, ...sources) => {
+  return Object.assign(target, ...sources);
+};
+
+obj.merge = (target, ...sources) => {
+  for (const source of sources) {
+    for (const [key, value] of Object.entries(source)) {
+      if (isPlainObject(value) && isPlainObject(target[key])) {
+        merge(target[key], value);
+      } else {
+        target[key] = value;
+      }
+    }
+  }
+
+  return target;
+};
+
+obj.deleteByPath = (object, path) => {
+  const { target, key } = obj.resolvePath(object, path);
+  delete target[key];
+
+  return object;
+};
+
 obj.getValueByPath = (object, dotKey) => {
   return obj.resolvePath(object, dotKey).value;
 };
+
+obj.hasPath = (object, path) => {
+  const { target, key } = obj.resolvePath(object, path);
+  return Object.hasOwn(target, key);
+};
+
 obj.setValueByPath = (object, path, value) => {
   const { target, key } = obj.resolvePath(object, path);
   target[key] = value;
 
   return object;
 };
-obj.toggleByPath = (object, dotKey) => {
-  const { target, key, value } = obj.resolvePath(object, dotKey);
+
+obj.toggleByPath = (object, path) => {
+  const { target, key, value } = obj.resolvePath(object, path);
 
   target[key] = isBool(value) ? !value
     : value === 'on'  ? 'off'
     : value === 'off' ? 'on'
     : value;
+
+  return object;
 };
 
 export { obj };
