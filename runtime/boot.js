65934f03bc0a5d030b49d9567fd3d5ac973b5cd2
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-06 13:59:28 +0200

    Update boot.js

--- runtime/boot.js
+++ runtime/boot.js
@@ -1,15 +1,17 @@
 // init.js
 
-import * as client from './client.js';
-import      config from './config.js';
+// :::::: IMPORTS
 
-import * as elements                      from '@aufbau/elements';
+import * as client     from './client.js';
+import      config     from './config.js';
+import * as elements   from '@aufbau/elements';
+import * as webfonts   from '@aufbau/webfonts';
+import adoptStylesheet from '@domina/methods/adoptStylesheet.js';
+import { deepMerge }   from '@pulgasari/obj';
+
 import { setConfig as setElementsConfig } from '@aufbau/elements/core/AufbauConfig.js';
-import * as webfonts                      from '@aufbau/webfonts';
 
-import { adoptStylesheet } from '@domina/methods/adoptStylesheet.js';
-import { deepMerge }       from '@pulgasari/obj';
-
+// :::::: 
 
 const  cssPath = 'https://code.pulgasari.dev/aufbau/css';
 const fontPath = 'https://code.pulgasari.dev/aufbau/webfonts/ttf';
@@ -30,23 +32,17 @@ function setConfig (options = {}) {
 
 // :::::: INIT
 
-const initAppearance = () => {
-  const { layout, look, reset, skin, theme } = config.css;
+const initElements = async ({ mode = 'auto' }) => {
+       if (mode === 'auto')      elements.autoloader();
+  else if (mode === 'all') await elements.registerAll();
+}
 
-  // load + apply font-files
-  webfonts.init(config.font)
-
-  // load + apply css-files
+const initStyleSheets = ({ layout, look, reset, skin, theme }) => {
   if (reset)  adoptStylesheet(`${cssPath}/aufbau.css`); // needs to be improved   
   if (layout) adoptStylesheet(`${cssPath}/layouts/${layout}.css`);
   if (look)   adoptStylesheet(`${cssPath}/looks/${look}.css`);
   if (skin)   adoptStylesheet(`${cssPath}/skins/${skin}.css`);
   if (theme)  adoptStylesheet(`${cssPath}/themes/${theme}.css`);
-}
-
-const initElements = async ({ mode = 'auto' }) => {
-       if (mode === 'auto')      elements.autoloader();
-  else if (mode === 'all') await elements.registerAll();
 }
 
 const initStylesheet = (bool) => bool && client.observeStylesheets();
@@ -56,7 +52,8 @@ async function boot (options = {}) {
   setConfig(options);
 
   if (typeof window !== 'undefined' && !isBooted) {
-    initAppearance ();
+    webfonts.init(config.font); // load + apply font-files
+    initStyleSheets(config.css);
     initElements   (config.elements);
     initStylesheet (config.stylesheet);
     isBooted = true;
