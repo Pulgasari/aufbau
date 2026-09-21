0ad96c41337d748202897f90727100ef43894d9a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-21 14:38:44 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,3 +4,20 @@ export const
 applyFilter = (await import('@aufbau/filter')).apply;
 
   
+
+import setStyleToken from '@domina/methods/setStyleToken.js';
+setStyleToken('theme', 'oled');
+
+class AufbauController {
+  set theme (value) { setStyleToken('theme', value); }
+  set theme (value) { setTheme(value); }
+
+  setTheme = async (id) => this.setStyleToken('theme', 'id');    
+
+  // lazy bridge to @domina
+  dom = {
+    getStyleToken: async (key,        target) => (await import('@domina/methods/getStyleToken.js')).default(key, target),
+    setStyleToken: async (key, value, target) => (await import('@domina/methods/setStyleToken.js')).default(key, value, target),       
+  }
+}
+
