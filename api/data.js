8f753a18e274b55a27cf4d3860742351f360f31b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-26 02:31:41 +0200

    Create data.js

---
+++ api/data.js
@@ -0,0 +1,22 @@
+// @aufbau/api/data.js
+
+import { fonts } from '@aufbau/webfonts/data.js';
+
+/*
+filters
+patterns
+stylescript
+webfonts
+*/
+
+const css = {
+  layouts : ['mobile'],
+  looks   : ['flat', 'rounded'],
+  skins   : ['monochrome'],
+  themes  : ['classic', 'oled', 'rainbow', 'zombie'],
+};
+
+
+
+export         { css, fonts };
+export default { css, fonts };
